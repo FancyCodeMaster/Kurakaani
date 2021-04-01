673c2e127c0b506cd2f615408e431f012d8467d4
FancyCodeMaster <fancycodemaster@gmail.com>
2021-04-01 20:59:27 +0545

    just started

--- Login/message.js
+++ Login/message.js
@@ -1,14 +1,23 @@
 let userBox = document.querySelectorAll(".user-info");
 
 
+userBox = Array.from(userBox);
+
+// To open the first message by default when the message section is completely loaded
+window.addEventListener("load" , () => {
+    userBox.map((el) => {
+        el.classList.remove("user-info-active");
+    })
+    userBox[0].classList.add("user-info-active");
+})
+
 // collections to make work easier
 let collections = {
 
 }
 
-userBox = Array.from(userBox);
-
-userBox.map((el , ind , arr) => {
+// When we click/select message container of one user , it unselects the message container of all other users
+userBox.map((el) => {
     el.addEventListener("click" , () => {
         userBox.map((el) => {
             el.classList.remove("user-info-active");
