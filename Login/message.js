f7cf12f58faf71c30b10e0a6f63b7702ef4604d1
FancyCodeMaster <fancycodemaster@gmail.com>
2021-04-06 15:32:08 +0545

    just started

--- Login/message.js
+++ Login/message.js
@@ -1,27 +1,13 @@
-let userBox = document.querySelectorAll(".user-info");
+let hamburgerIcon = document.querySelector(".hamburger-icon");
+let sideBar = document.querySelector(".online-preview-container");
+let backdrop = document.querySelector(".backdrop");
 
-
-userBox = Array.from(userBox);
-
-// To open the first message by default when the message section is completely loaded
-window.addEventListener("load" , () => {
-    userBox.map((el) => {
-        el.classList.remove("user-info-active");
-    })
-    userBox[0].classList.add("user-info-active");
+hamburgerIcon.addEventListener("click" , () => {
+    backdrop.classList.add("backdrop-active");
+    sideBar.classList.add("online-preview-container-active");
 })
 
-// collections to make work easier
-let collections = {
-
-}
-
-// When we click/select message container of one user , it unselects the message container of all other users
-userBox.map((el) => {
-    el.addEventListener("click" , () => {
-        userBox.map((el) => {
-            el.classList.remove("user-info-active");
-        })
-        el.classList.add("user-info-active");
-    })
+backdrop.addEventListener("click" , () => {
+    backdrop.classList.remove("backdrop-active");
+    sideBar.classList.remove("online-preview-container-active");
 })
